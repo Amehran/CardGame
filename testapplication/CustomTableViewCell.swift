d07f89c1c4822114578fa8325ee80afab9db7916
Armin Mehran <a.mehran@gmail.com>
2018-01-16 13:32:12 -0500

    first commit

---
+++ testapplication/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  testapplication
+//
+//  Created by Armin Mehran on 2016-04-29.
+//  Copyright © 2016 Armin Mehran. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
